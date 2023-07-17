3f7cf62cba6a7343f6bb89530c308da0f0555719
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-07-16 23:23:45 -0300

    ...

--- ListadeTarefas/src/Lista.jsx
+++ ListadeTarefas/src/Lista.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import iconDel from './assets/btnDel.svg';
 import iconAdd from './assets/btnAdd.svg';
 import './Lista.css';
@@ -6,11 +6,18 @@ import './Lista.css';
 
 function Lista(){
 
+    const listaStorage = localStorage.getItem('Lista');
+
+
     //Estado inicial da lista, um array zerado. 
-    const [lista, setLista] = useState([]);
+    const [lista, setLista] = useState(listaStorage ? JSON.parse(listaStorage) : []);
 
     //Pegará o valor digitado, vai colocar na lista e vai limpar esse campo
     const [novoItem, setNovoItem] = useState("");
+
+    useEffect(()=>{
+        localStorage.setItem('Lista', JSON.stringify(lista))
+    }, [lista])
 
 
     function adicionaItem(form){
@@ -21,7 +28,19 @@ function Lista(){
         setLista([...lista, {text: novoItem, isCompleted: false}]);
         setNovoItem("");
         document.getElementById('input-entrada').focus();
-
+    }
+    function clicou(index){
+        const listaAux = [...lista];
+        listaAux[index].isCompleted = !listaAux[index].isCompleted;
+        setLista(listaAux);
+    }
+    function deleta(index){
+        const listaAux = [...lista];
+        listaAux.splice(index,1);
+        setLista(listaAux);
+    }
+    function deletaTodas(){
+        setLista([]);
     }
 
     return(
@@ -44,13 +63,19 @@ function Lista(){
                         <h1>Lista Vazia</h1>
                     :
                         lista.map((item, index)=>(
-                            <div className="item">
-                            <span className="tarefaAtiva">{item.text}</span>
-                            <button className="btnExcluir">Exluir</button>
+                            <div key={index} className="item">
+                            <span onClick={()=>{clicou(index)}} className={item.isCompleted ? "tarefaFeita" : "tarefaAtiva"}>{item.text}</span>
+                            <button onClick={() => {deleta(index)}} className="btnExcluir">Exluir</button>
                             </div>
-                        ))}
+                        ))
+                }
+
+                {
+                    lista.length > 0 &&
+                    <button onClick={() => deletaTodas()} className="btnDeleteAll">Deletar Todas</button>
+                }
               
-                <button className="btnDeleteAll">Deletar Todas</button>
+                
             </div>
         </div>
     )
